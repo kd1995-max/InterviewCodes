a146bd48aa6005f9371c12530ad56441c681b12d
kd1995-max <75378214+kd1995-max@users.noreply.github.com>
2022-06-10 18:27:45 +0530

    Update CountTheReversals.java

--- CountTheReversals.java
+++ CountTheReversals.java
@@ -1,22 +1,19 @@
-class Sol
+int countRev (string s)
 {
-    int countRev (String s)
-    {
-        // your code here  
-        if(s.length()%2!=0) return -1;
+    // your code here
+    if(s.length()%2!=0) return -1;
         
         int open=0,close=0,rev=0;
         
         for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i)=='{') open++;
+            if(s[i]=='{') open++;
             else
             {
                 if(open==0) close++;
                 else open--;
             }
         }
-        rev=(int)(Math.ceil(open/2.0) + Math.ceil(close/2.0)); 
+        rev=(int)(ceil(open/2.0) + ceil(close/2.0)); 
         return rev;
-    }
 }
